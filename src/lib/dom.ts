f90b2c21d7baaa0087d45ad95be51100ea5fc338
wirekang <wirekang@gmail.com>
2024-05-15 20:22:58 +0900

    add key event patch

--- src/lib/dom.ts
+++ src/lib/dom.ts
@@ -1,3 +1,5 @@
+import { logger } from "./log";
+
 type KeyEvent = {
   key: string;
   ctrl: boolean;
@@ -6,15 +8,31 @@ type KeyEvent = {
 
 type KeyEventListener = (e: KeyEvent) => boolean;
 
+const patch = new Map<string, string | null>([
+  [" ", "space"],
+  ["control", null],
+  ["shift", null],
+  ["alt", null],
+  ["meta", null],
+]);
+
+const l = logger("keyevent");
 export function initKeyboardEvent(listener: KeyEventListener) {
   document.addEventListener("keydown", (e) => {
     if (e.repeat) {
       return;
     }
-    const ke = { key: e.key.toLowerCase(), ctrl: e.ctrlKey, shift: e.shiftKey };
-    if (ke.key === " ") {
-      ke.key = "space";
+    let key = e.key.toLowerCase();
+    const patchedKey = patch.get(key);
+    if (patchedKey === null) {
+      l.debug(`ignore '${key}'`);
+      return;
     }
+    if (patchedKey !== undefined) {
+      l.debug(`patch '${key}'->'${patchedKey}'`);
+      key = patchedKey;
+    }
+    const ke = { key, ctrl: e.ctrlKey, shift: e.shiftKey };
     if (listener(ke)) {
       e.preventDefault();
       e.stopPropagation();

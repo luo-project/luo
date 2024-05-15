ae9958c09e66f793895b797732f0ccb5e22fe277
VertexToEdge <vertex@g.cnu.ac.kr>
2024-05-15 22:21:49 +0900

    add available function in command redo, undo

--- src/lib/commands/graph-redo.ts
+++ src/lib/commands/graph-redo.ts
@@ -2,6 +2,12 @@ import type { CommandDefinition } from "../command";
 
 export const def: CommandDefinition = {
   description: "redo for graph modification command",
+  available: (state) => {
+    if (state.graph.current < state.graph.snapshots.length - 1) {
+      return { result: true };
+    }
+    return { reason: "There are no history for redo", result: false };
+  },
   async func(state) {
     if (state.graph.current < state.graph.snapshots.length - 1) {
       state.graph.current++;
--- src/lib/commands/graph-undo.ts
+++ src/lib/commands/graph-undo.ts
@@ -2,6 +2,12 @@ import type { CommandDefinition } from "../command";
 
 export const def: CommandDefinition = {
   description: "undo for graph modification command",
+  available: (state) => {
+    if (state.graph.current > 0) {
+      return { result: true };
+    }
+    return { reason: "There are no history for undo", result: false };
+  },
   async func(state) {
     if (state.graph.current > 0) {
       state.graph.current--;

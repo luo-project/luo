c252dfc689dc8abe88195eb60afbd2388dcf7ad0
Seongbin Hong <37045096+VertexToEdge@users.noreply.github.com>
2024-05-15 21:24:38 +0900

    add getCurrentCommand for Hook (#24)
    * add getCurrentCommand function for ref of state
    
    * fix History Hook's Hook function parameter

--- src/lib/hooks/15-history.ts
+++ src/lib/hooks/15-history.ts
@@ -1,9 +1,8 @@
 import type { HookDefinition } from "../hook";
-import { State } from "../state";
 import { deepCopy } from "../utils";
 
 export const def: HookDefinition = {
-  func(state: State, config) {
+  func(state, config, ref) {
     if (state.graph.current < state.graph.snapshots.length - 1) {
       // remove future snapshots
       state.graph.snapshots = state.graph.snapshots.slice(0, state.graph.current + 1);

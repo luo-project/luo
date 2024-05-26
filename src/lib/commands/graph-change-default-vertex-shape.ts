c497acb1c3afa9a1d4f217c1d46312564eee4b2c
VertexToEdge <vertex@g.cnu.ac.kr>
2024-05-26 14:38:01 +0900

    add change vertex's default shape

---
+++ src/lib/commands/graph-change-default-vertex-shape.ts
@@ -0,0 +1,13 @@
+import type { CommandDefinition } from "../command";
+import { VERTEX_SHAPES } from "../constants";
+import { Edge, GraphElement, isEdge, isVertex } from "../graph";
+
+export const def: CommandDefinition = {
+  description: "Change vertex's default shape.",
+  func(state, config, ctx) {
+    const defaultVertexShape = state.defaultVertex.shape;
+    const shapeIndex = VERTEX_SHAPES.indexOf(defaultVertexShape);
+    const nextShapeIndex = (shapeIndex + 1) % VERTEX_SHAPES.length;
+    state.defaultVertex.shape = VERTEX_SHAPES[nextShapeIndex];
+  },
+};
